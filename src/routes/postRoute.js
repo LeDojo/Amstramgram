d359baa999fcda51d319015e83b3acffc3d22bb1
FreezyXV <i_841@mail.ru>
2024-01-03 17:11:28 +0100

    Clean structure project

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,21 +1,30 @@
+// Importe les fonctions du contrôleur postController
 const {
   getAllPosts,
   createPost,
   showPost,
   updatePost,
-  deletePost
+  deletePost,
 } = require("../controllers/postController");
 
+const { uploadImage } = require("../middlewares/multer");
+// Importe le module Router d'Express
 const postRouter = require("express").Router();
 
+// Route pour récupérer tous les posts
 postRouter.get("/all", getAllPosts);
 
-postRouter.post("/add", createPost);
+// Route pour créer un nouveau post
+postRouter.post("/add", uploadImage.single("image"), createPost);
 
+// Route pour récupérer un post spécifique en fonction de son ID
 postRouter.get("/:id", showPost);
 
+// Route pour mettre à jour un post spécifique en fonction de son ID
 postRouter.put("/edit/:id", updatePost);
 
-postRouter.delete("/delete/:id", deletePost)
+// Route pour supprimer un post spécifique en fonction de son ID
+postRouter.delete("/delete/:id", deletePost);
 
+// Exporte le routeur pour l'utiliser dans d'autres fichiers
 module.exports = postRouter;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,14 @@
+// Importe les fonctions du contrôleur userController
 const { store, login } = require("../controllers/userController");
 
+// Importe le module Router d'Express
 const userRouter = require("express").Router();
 
+// Route pour gérer l'inscription d'un utilisateur
 userRouter.post("/inscription", store);
+
+// Route pour gérer la connexion d'un utilisateur
 userRouter.post("/connexion", login);
 
+// Exporte le routeur pour l'utiliser dans d'autres fichiers
 module.exports = userRouter;
