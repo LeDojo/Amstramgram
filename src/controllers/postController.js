c487b4107d7b979a3d7b39797a4e1c8b7c35c573
Mehdi <mehdibad93100@gmail.com>
2024-01-03 11:57:55 +0100

    adding create and show post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,12 +1,32 @@
 const Post = require("../models/postModel");
 
 const getAllPosts = async (req, res) => {
-    try {
-        const posts = await Post.find()
-        res.json(posts)
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-}
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
 
-module.exports = {getAllPosts}
+const createPost = async (req, res) => {
+  const { description, image } = req.body;
+  try {
+    const newPost = await Post.create({ description, image });
+    res.json(newPost);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+const showPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+
+module.exports = { getAllPosts, createPost, showPost };
--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,7 +1,11 @@
-const { getAllPosts } = require("../controllers/postController");
+const { getAllPosts, createPost, showPost } = require("../controllers/postController");
 
 const postRouter = require("express").Router();
 
 postRouter.get("/all", getAllPosts);
+
+postRouter.post("/add", createPost);
+
+postRouter.get("/:id", showPost);
 
 module.exports = postRouter;
